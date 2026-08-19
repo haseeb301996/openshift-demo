cc82780399e85fe1de8f08353779dc47e0586cc5
haseeb301996 <haseeb301996@gmail.com>
2026-08-19 18:33:02 +0530

    NEW commit 3

--- src/main/java/com/example/openshiftdemo/controller/HelloController.java
+++ src/main/java/com/example/openshiftdemo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from OpenShift changes 2!";
+        return "Hello from OpenShift changes 3!";
     }
 }
