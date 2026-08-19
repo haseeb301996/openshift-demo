4e237ceb5e73a3ca0bd87e52567fee7170467e77
haseeb301996 <haseeb301996@gmail.com>
2026-08-19 19:11:48 +0530

    Update HelloController.java
    hahahah

--- src/main/java/com/example/openshiftdemo/controller/HelloController.java
+++ src/main/java/com/example/openshiftdemo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "checking ocp deployment!";
+        return "checking ocp deployment! hahahahahahahahh ";
     }
 }
